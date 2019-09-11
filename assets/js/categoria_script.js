92123b426f7630cfeea540c3febebb012c8f7d25
MatFreitas <matheusfs2@al.insper.edu.br>
2019-09-11 09:30:10 -0300

    Conectando com Firebase

---
+++ assets/js/categoria_script.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+		let db = coDesConnect('https://console.firebase.google.com/project/portfolio-engenharia1/database/portfolio-engenharia1/data')
+})
---
+++ assets/js/index_script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	let db = coDesConnect('https://console.firebase.google.com/project/portfolio-engenharia1/database/portfolio-engenharia1/data')
+})
