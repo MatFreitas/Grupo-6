4c3fc9daff7827e6a8d95930389368b8dc1e83a0
MatFreitas <matheusfs2@al.insper.edu.br>
2019-09-17 09:36:20 -0300

    TUDO

--- assets/js/projeto_script.js
+++ assets/js/projeto_script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-		let db = coDesConnect('https://console.firebase.google.com/project/portfolio-engenharia1/database/portfolio-engenharia1/data')
+
+	let db = coDesConnect('https://portfolio-engenharia1.firebaseio.com')
+
+	db.download('/portfolio/calculo/projetos', function(data) {
+
+		for(let key in data) {	
+
+			console.log(data[key])
+			
+		coDesReplace('.paragraph', data[key])
+  	}
+  })
 })
